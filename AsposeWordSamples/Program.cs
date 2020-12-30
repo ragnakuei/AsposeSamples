81f5ff1e462c561f9b9fa13aec8ec03c18cf5403
Kuei <ragnakuei@livemail.tw>
2020-12-30 09:27:06 +0800

    Add project files.

---
+++ AsposeWordSamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsposeWordSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
